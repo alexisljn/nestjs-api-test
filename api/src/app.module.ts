cab74ce3b7b25f5c5bcc87af82c2926f546f3102
alexisljn <alexis.lejeune@gmx.fr>
2023-02-06 19:18:41 +0100

    delete useless files related to app domain

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,4 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {ProductsModule} from "./products/products.module";
 import {MongooseModule} from "@nestjs/mongoose";
 import {ConfigModule} from "@nestjs/config";
@@ -11,8 +9,6 @@ import {ConfigModule} from "@nestjs/config";
         MongooseModule.forRoot(`mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongo:27017/`,
             {dbName: process.env.MONGO_APP_DATABASE}),
         ProductsModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+    ]
 })
 export class AppModule {}
