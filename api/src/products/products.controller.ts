2850c277df78c22933b01cb2928707ca00c7315d
alexisljn <alexis.lejeune@gmx.fr>
2023-02-06 18:03:08 +0100

    create GET products/ route handler

--- api/src/products/products.controller.ts
+++ api/src/products/products.controller.ts
@@ -1,14 +1,28 @@
 import {
+    BadRequestException,
     Controller,
     Get,
     NotFoundException,
     Param,
+    Query
 } from "@nestjs/common";
 import {ProductsService} from "./products.service";
+import {ProductQueryArgs} from "./types/products.types";
 
 @Controller('products')
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
+
+    @Get()
+    async findAll(@Query() queryArgs: ProductQueryArgs) {
+        if (!this.productsService.validateQueryArgs(queryArgs)) {
+            throw new BadRequestException();
+        }
+
+        const queryFilters = this.productsService.generateQueryFilters(queryArgs);
+
+        return await this.productsService.findAll(queryFilters)
+    }
 
     @Get(':code')
     async findOne(@Param('code') code: string) {
