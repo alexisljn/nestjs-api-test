7b1a79f75cbeed66c4be6aadf16983a2299ad6d4
alexisljn <alexis.lejeune@gmx.fr>
2023-02-06 17:58:18 +0100

    create findAll() in ProductsService

--- api/src/products/products.service.ts
+++ api/src/products/products.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import {Product, ProductDocument} from "./schemas/product.schema";
+import {InjectModel} from "@nestjs/mongoose";
+import {Model} from "mongoose";
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) {}
+
+    async findByCode(code: string): Promise<Product | null> {
+        return this.productModel.findOne({code}).exec();
+    }
+
+}
