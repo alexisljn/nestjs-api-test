41e0784b2a4475c1a0e5b9e689366e6e6827ae5a
alexisljn <alexis.lejeune@gmx.fr>
2023-02-06 17:59:56 +0100

    create GET products/:code route handler

--- api/src/products/products.controller.ts
+++ api/src/products/products.controller.ts
@@ -1,4 +1,23 @@
-import {Controller} from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    NotFoundException,
+    Param,
+} from "@nestjs/common";
+import {ProductsService} from "./products.service";
 
-@Controller()
-export class ProductsController {}
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {}
+
+    @Get(':code')
+    async findOne(@Param('code') code: string) {
+        const product = await this.productsService.findByCode(code);
+
+        if (product === null) {
+            throw new NotFoundException();
+        }
+
+        return product;
+    }
+}
